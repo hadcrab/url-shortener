bb91f2a69b012d55d102c2ea39f0b0db360016c8
Mihail <mihailsorin58@gmail.com>
2025-11-01 16:24:32 +0500

    add db.js

---
+++ db.js
@@ -0,0 +1,13 @@
+import Database from "better-sqlite3";
+
+const db = new Database("./data.db");
+
+db.exec(`
+  CREATE TABLE IF NOT EXIST links (
+    id TEXT PRIMARY KEY,
+    target_url TEXT NOT NULL,
+    created_at TEXT DEFAULT CURRENT_TIMESTAMP
+  );
+`);
+
+export default db;
